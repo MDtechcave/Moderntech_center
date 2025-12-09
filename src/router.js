2822fd2791182c6d90c7b0f40e72b69aaa905b88
mihle-aviwe-dudumashe <mihleaviwe6@gmail.com>
2025-12-09 16:10:12 +0200

    Renamed Payslip.vue to Payroll.vue, fixed the router

--- src/router.js
+++ src/router.js
@@ -2,16 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HrHome from './components/HrHome.vue';
 import About from './components/About.vue';
 
-import Payslip from './components/Payslip.vue';
 import employees from './components/employees.vue';
 import attendance from './components/attendance.vue';
+import Payroll from './components/Payroll.vue';
 
 const routes = [
   { path:'/',  component: HrHome },
   {path: '/about', component: About},
-  { path: '/payslip', component: Payslip },
   {path: '/employees', component: employees},
   {path: '/attendance', component: attendance},
+  {path: '/payroll', component: Payroll},
 
 ];
 
