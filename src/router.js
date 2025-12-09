54a3cbe24210402677f4009ffefe96850a9623e3
simbonilenduneni03 <emihledumo@gmail.com>
2025-12-09 14:55:43 +0200

    resolve router conflict

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import HrHome from './components/HrHome.vue';
 import About from './components/About.vue';
 import Payslip from './components/Payslip.vue';
 import employees from './components/employees.vue';
+import attendance from './components/attendance.vue';
 
 const routes = [
   { path:'/',  component: HrHome },
   {path: '/about', component: About},
   { path: '/payslip', component: Payslip },
-  {path: '/employees', component: employees}
+  {path: '/employees', component: employees},
+  {path: '/attendance', component: attendance},
 
 ];
 
