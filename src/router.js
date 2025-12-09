cd2366e46bf640a9ca854a61e906eca552b4d806
mihle-aviwe-dudumashe <mihleaviwe6@gmail.com>
2025-12-09 15:55:39 +0200

    Resolve conflict problems

--- src/router.js
+++ src/router.js
@@ -1,23 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HrHome from './components/HrHome.vue';
 import About from './components/About.vue';
-<<<<<<< HEAD
 
-=======
 import Payslip from './components/Payslip.vue';
 import employees from './components/employees.vue';
 import attendance from './components/attendance.vue';
->>>>>>> 54a3cbe24210402677f4009ffefe96850a9623e3
 
 const routes = [
   { path:'/',  component: HrHome },
   {path: '/about', component: About},
-<<<<<<< HEAD
-=======
   { path: '/payslip', component: Payslip },
   {path: '/employees', component: employees},
   {path: '/attendance', component: attendance},
->>>>>>> 54a3cbe24210402677f4009ffefe96850a9623e3
 
 ];
 
