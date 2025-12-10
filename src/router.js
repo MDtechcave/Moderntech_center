b1a9acd0b4f29b02bca5fc19cfeeca44ccb604a5
mihle-aviwe-dudumashe <mihleaviwe6@gmail.com>
2025-12-10 15:51:10 +0200

    Redid the login.vue, modified payroll.vue, renamed employeeinfo.js and fixed router

--- src/router.js
+++ src/router.js
@@ -34,11 +34,25 @@ import Payroll from './components/Payroll.vue';
 const routes = [
   { path: '/login', component: Login },
   {path: '/workreviews', component: workreviews},
-  { path: '/', component: HrHome },
-  { path: '/about', component: About },
-  { path: '/employees', component: employees },
-  { path: '/attendance', component: attendance },
-  { path: '/payroll', component: Payroll },
+  { path: '/',
+     component: HrHome ,
+    meta:{requiresAuth: true}
+  },
+  { path: '/about', component: About, 
+    meta: {requiresAuth: true}
+  },
+  { path: '/employees',
+     component: employees ,
+     meta: {requiresAuth: true}
+  },
+  { path: '/attendance',
+     component: attendance,
+     meta: {requiresAuth: true}
+    },
+  { path: '/payroll',
+     component: Payroll,
+     meta: {requiresAuth: true}
+     },
 ];
 
 const router = createRouter({
@@ -47,12 +61,12 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  const loggedIn = localStorage.getItem("loggedIn");
+  const isloggedIn = localStorage.getItem("user");
 
-  if (to.meta.requiresAuth && !loggedIn) {
-    next('/');
+  if (to.meta.requiresAuth && !isloggedIn) {
+    next('/login'); //sends you to login
   } else {
-    next()
+    next(); //allows access
   }
 });
 
